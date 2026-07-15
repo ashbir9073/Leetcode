7c28c55a5fcb8acfa3339977dedc354328706dc4
ashbir9073 <ashbirshaw9073@gmail.com>
2026-07-15 22:28:12 +0530

    new

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,5 @@
 void main() {
         System.out.println("Hello and welcome!");
-        System.out.println("Hello World!");
-        System.out.println("Mayank New Model");
         System.out.println("Ashbir");
     }
 
